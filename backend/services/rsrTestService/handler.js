3bda4abb383c27d13ed5c08f373d7a941ae9adf1
Ravindra Singh Rathor <ravindra@mypay.co.uk>
2021-01-12 11:52:44 +0530

    Triggering the build again.

--- backend/services/rsrTestService/handler.js
+++ backend/services/rsrTestService/handler.js
@@ -5,7 +5,8 @@ module.exports.hello = async (event) => {
         statusCode: 200,
         body: JSON.stringify(
             {
-                message: 'Go Serverless v1.0! Your function executed successfully! :: Message from Ravindra'
+                message: 'Go Serverless v1.0! Your function executed successfully! :: Message from Ravindra',
+                misaVersion: 5
             },
             null,
             2
